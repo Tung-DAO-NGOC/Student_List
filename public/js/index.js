9beb1ab173d70ff1d2d9676790107de6d85e7717
Tung-DAO-NGOC <tungdn30.weo@gmail.com>
2021-12-04 10:02:12 +0700

    Render list

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,10 @@
 
 const elTBody = document.querySelector("tbody");
 
-let users = [];
+// Global
 
+let gUser = [];
+let userCheckNull = [];
 // API
 
 function getListAPI() {
@@ -12,14 +14,46 @@ function getListAPI() {
 async function getList() {
 	try {
 		const res = await getListAPI();
-		users = res.data;
-		console.log(users);
+		const userList = res.data;
+		renderBody(userList);
 	} catch (error) {
 		console.log(error);
 	}
 }
 
 // Function
+
+function renderBody(list) {
+	clearBody();
+
+	if (list.length === 0) {
+		elTBody.innerHTML = `<tr><td colspan="5"><div class="noUser">There is no user</tr>`;
+		return;
+	} else {
+		list.sort((a, b) => -(a.id - b.id));
+
+		list.forEach((user) => {
+			elTBody.innerHTML += `
+			<tr>
+				<td>${user.name}</td>
+				<td>${user.birthday}</td>
+				<td>${user.email}</td>
+				<td>${user.phone}</td>
+				<td>
+					<a href="/edit.html?id=${user.id}" class="text-info"><i class="fa fa-edit"></i> Chỉnh sửa</a>
+					|
+					<a class="text-danger" onclick="deleteUser(${user.id})"><i class="fa fa-trash-alt"></i> Xóa</a>
+				</td>
+			</tr>`;
+		});
+	}
+}
+
+function deleteUser(id) {
+	console.log(`Deleting ${id}`);
+}
+
+function sort(arrayUser) {}
 
 function clearBody() {
 	elTBody.innerHTML = "";
@@ -28,5 +62,5 @@ function clearBody() {
 // Main
 
 window.onload = () => {
-	clearBody();
+	getList();
 };
